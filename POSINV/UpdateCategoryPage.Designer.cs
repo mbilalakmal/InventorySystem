99a3ca77e3ace615108d7dc398f8adf64ee94f94
Bilal <mbilalakmal@gmail.com>
2020-01-08 04:57:37 +0500

    Added delete for product, brand, and category. Added update for category

---
+++ POSINV/UpdateCategoryPage.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace POSINV
+{
+    partial class UpdateCategoryPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateCategoryPage));
+            this.btnUpdateCategory = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.textName = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.labelName = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // btnUpdateCategory
+            // 
+            this.btnUpdateCategory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdateCategory.AutoSize = true;
+            this.btnUpdateCategory.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnUpdateCategory.Depth = 0;
+            this.btnUpdateCategory.Icon = null;
+            this.btnUpdateCategory.Location = new System.Drawing.Point(19, 146);
+            this.btnUpdateCategory.Margin = new System.Windows.Forms.Padding(10);
+            this.btnUpdateCategory.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnUpdateCategory.Name = "btnUpdateCategory";
+            this.btnUpdateCategory.Padding = new System.Windows.Forms.Padding(10);
+            this.btnUpdateCategory.Primary = true;
+            this.btnUpdateCategory.Size = new System.Drawing.Size(128, 36);
+            this.btnUpdateCategory.TabIndex = 8;
+            this.btnUpdateCategory.Text = "Save Category";
+            this.btnUpdateCategory.UseVisualStyleBackColor = true;
+            this.btnUpdateCategory.Click += new System.EventHandler(this.btnUpdateCategory_Click);
+            // 
+            // textName
+            // 
+            this.textName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textName.Depth = 0;
+            this.textName.Hint = "Name";
+            this.textName.Location = new System.Drawing.Point(19, 110);
+            this.textName.MaxLength = 100;
+            this.textName.MouseState = MaterialSkin.MouseState.HOVER;
+            this.textName.Name = "textName";
+            this.textName.PasswordChar = '\0';
+            this.textName.SelectedText = "";
+            this.textName.SelectionLength = 0;
+            this.textName.SelectionStart = 0;
+            this.textName.Size = new System.Drawing.Size(154, 23);
+            this.textName.TabIndex = 7;
+            this.textName.TabStop = false;
+            this.textName.UseSystemPasswordChar = false;
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelName.AutoSize = true;
+            this.labelName.Depth = 0;
+            this.labelName.Font = new System.Drawing.Font("Roboto", 11F);
+            this.labelName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelName.Location = new System.Drawing.Point(15, 85);
+            this.labelName.Margin = new System.Windows.Forms.Padding(3);
+            this.labelName.MaximumSize = new System.Drawing.Size(210, 38);
+            this.labelName.MouseState = MaterialSkin.MouseState.HOVER;
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(123, 19);
+            this.labelName.TabIndex = 6;
+            this.labelName.Text = "Category Name *";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UpdateCategoryPage
+            // 
+            this.AcceptButton = this.btnUpdateCategory;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 192);
+            this.Controls.Add(this.btnUpdateCategory);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.labelName);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(240, 240);
+            this.MinimumSize = new System.Drawing.Size(192, 192);
+            this.Name = "UpdateCategoryPage";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Update Category";
+            this.Load += new System.EventHandler(this.UpdateCategoryPage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton btnUpdateCategory;
+        private MaterialSkin.Controls.MaterialSingleLineTextField textName;
+        private MaterialSkin.Controls.MaterialLabel labelName;
+    }
+}
---
+++ POSINV/UpdateCategoryPage.cs
@@ -0,0 +1,58 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSINV
+{
+    public partial class UpdateCategoryPage : MaterialForm
+    {
+        public CategoryModel category { get; private set; }
+
+        public UpdateCategoryPage(CategoryModel categoryOld)
+        {
+            InitializeComponent();
+
+            //materialSkin
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Teal500, Primary.Teal700, Primary.Teal100, Accent.Teal400, TextShade.WHITE
+            );
+
+            category = categoryOld;
+
+            textName.Text = category.CategoryName;
+        }
+
+        private void UpdateCategoryPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdateCategory_Click(object sender, EventArgs e)
+        {
+            //check if name is ok then update
+            category.CategoryName = textName.Text.Trim();
+
+            if( category.CategoryName.Length == 0)
+            {   //problem
+                MessageBox.Show("Category Name can not be empty");
+                return;
+            }
+            //update in db, combobox and datagrid
+            SQLiteDataAccess.UpdateCategory(category.CategoryId, category.CategoryName);
+
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
