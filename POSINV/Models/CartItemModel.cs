145d05e6295e85f0439ee3f12469aff739f39492
Bilal <mbilalakmal@gmail.com>
2020-01-15 18:45:58 +0500

    Added Models for CartItems (containing Product & quantity) and Sale (containing misc and total charges)

---
+++ POSINV/Models/CartItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSINV.Models
+{
+    public class CartItemModel
+    {
+        public ProductModel product { get; set; }
+        public int productQuantity { get; set; }
+    }
+}
---
+++ POSINV/Models/SaleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSINV.Models
+{
+    public class SaleModel
+    {
+        public int SaleId { get; set; }
+        public DateTime SaleDate { get; set; }
+        public decimal MiscPrice { get; set; }
+        public decimal SaleTotal { get; set; }
+    }
+}
