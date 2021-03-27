df9981f30ce191828bb2c847f165308bfceca980
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-03-27 19:52:12 +0500

    Added screenshots

--- POSINV/Properties/AssemblyInfo.cs
+++ POSINV/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("POSINV")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Point-of-Sale and inventory managment solution.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mbilalakmal")]
 [assembly: AssemblyProduct("POSINV")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
